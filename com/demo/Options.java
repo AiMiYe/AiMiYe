0714410ba770641e27ceed7243efe544a37534e9
AiMiYe <532811131@qq.com>
2020-10-05 22:18:59 +0800

    ## 第一次提交Demo

---
+++ com/demo/Options.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD,ElementType.METHOD, ElementType.TYPE,ElementType.CONSTRUCTOR})
+public @interface Options {
+    String value();
+}
